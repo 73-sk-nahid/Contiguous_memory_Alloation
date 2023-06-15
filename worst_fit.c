eb17f24f015821c8041b6d83281c49959b265277
GHOST <nahidsheikh2001@gmail.com>
2023-06-15 18:40:43 +0600

    Worst_Fit_Memory_Allocation

--- worst_fit.c
+++ worst_fit.c
@@ -1,42 +1,37 @@
-#include<stdio.h>
-#define max 25
+#include <stdio.h>
+#define MAX 25
 
-int main()
-{
-    int frag[max], b[max], f[max], i, j, nb, nf, temp, highest = 0;
-    static int bf[max], ff[max];
+int main() {
+    int frag[MAX], b[MAX], f[MAX], i, j, nb, nf, temp, highest = 0;
+    static int bf[MAX], ff[MAX];
 
-    printf("\nEnter the number of blocks: ");
+    printf("\nEnter the number of blocks:");
     scanf("%d", &nb);
 
-    printf("Enter the number of files: ");
+    printf("Enter the number of files:");
     scanf("%d", &nf);
 
     printf("\nEnter the size of the blocks:\n");
-    for(i = 0; i < nb; i++)
-    {
-        printf("Block %d: ", i);
+    for (i = 1; i <= nb; i++) {
+        printf("Block %d:", i);
         scanf("%d", &b[i]);
     }
 
-    printf("\nEnter the size of the files:\n");
-    for(i = 0; i < nf; i++)
-    {
-        printf("File %d: ", i);
+    printf("Enter the size of the files:\n");
+    for (i = 1; i <= nf; i++) {
+        printf("File %d:", i);
         scanf("%d", &f[i]);
     }
 
-    for(i = 0; i < nf; i++)
-    {
-        for(j = 0; j < nb; j++)
-        {
-            if(bf[j] == 0)
-            {
+    for (i = 1; i <= nf; i++) {
+        for (j = 1; j <= nb; j++) {
+            if (bf[j] != 1) {
                 temp = b[j] - f[i];
-                if(temp >= 0 && highest < temp)
-                {
-                    ff[i] = j;
-                    highest = temp;
+                if (temp >= 0) {
+                    if (highest < temp) {
+                        ff[i] = j;
+                        highest = temp;
+                    }
                 }
             }
         }
@@ -45,11 +40,9 @@ int main()
         highest = 0;
     }
 
-    printf("\nFile_no \tFile_size \tBlock_no \tBlock_size \tFragment\n");
-    for(i = 1; i <= nf; i++)
-    {
-        printf("%d\t\t%d\t\t%d\t\t%d\t\t%d\n", i, f[i], ff[i], b[ff[i]], frag[i]);
-    }
+    printf("\nFile_no \tFile_size \tBlock_no \tBlock_size \tFragment");
+    for (i = 1; i <= nf; i++)
+        printf("\n%d\t\t%d\t\t%d\t\t%d\t\t%d", i, f[i], ff[i], b[ff[i]], frag[i]);
 
     return 0;
 }
